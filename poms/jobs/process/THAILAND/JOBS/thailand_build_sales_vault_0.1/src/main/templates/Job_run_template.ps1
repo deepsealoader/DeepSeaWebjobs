b08b090b96e7f3c9eb103a459cc3d59a47c6a70d
Romuald Rousseau <romualdrousseau@gmail.com>
2019-05-26 14:49:59 +0800

    Fix geography coefficient issue

--- poms/jobs/process/THAILAND/JOBS/thailand_build_sales_vault_0.1/src/main/templates/Job_run_template.ps1
+++ poms/jobs/process/THAILAND/JOBS/thailand_build_sales_vault_0.1/src/main/templates/Job_run_template.ps1
@@ -1,3 +1,3 @@
 $fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 cd $fileDir
-java '-Xms256M' '-Xmx1024M' -cp '.;../lib/routines.jar;../lib/dom4j-1.6.1.jar;../lib/log4j-1.2.16.jar;thailand_build_sales_vault_0_1.jar;dsendexception_0_1.jar;thailand_build_sales_hubs_0_1.jar;sat_sales_0_1.jar;generate_customer_diff_0_1.jar;link_sales_0_1.jar;thailand_build_sales_sats_0_1.jar;hub_sales_0_1.jar;thailand_build_sales_links_0_1.jar;' deepsea.thailand_build_sales_vault_0_1.THAILAND_BUILD_SALES_VAULT  --context=Production %* 
+java '-Xms256M' '-Xmx1024M' -cp '.;../lib/routines.jar;../lib/dom4j-1.6.1.jar;../lib/log4j-1.2.16.jar;thailand_build_sales_vault_0_1.jar;dsendexception_0_1.jar;thailand_build_sales_hubs_0_1.jar;sat_sales_0_1.jar;generate_customer_diff_0_1.jar;link_sales_0_1.jar;thailand_build_sales_sats_0_1.jar;hub_sales_0_1.jar;thailand_build_sales_links_0_1.jar;' deepsea.thailand_build_sales_vault_0_1.THAILAND_BUILD_SALES_VAULT  --context=Development %* 
