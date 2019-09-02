5cafa3eeef83fc6541aa7e5e3fccc7867d9831ba
Romuald Rousseau <romualdrousseau@gmail.com>
2019-09-02 14:45:10 +0800

    Clean up and refactor

--- poms/jobs/process/COMMON/LIB/dsendexception_0.1/src/main/templates/Job_run_template.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-ROOT_PATH=`pwd`
-java -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/dom4j-1.6.1.jar:$ROOT_PATH/../lib/jersey-client-1.4.jar:$ROOT_PATH/../lib/jersey-core-1.4.jar:$ROOT_PATH/../lib/log4j-1.2.16.jar:$ROOT_PATH/dsendexception_0_1.jar: deepsea.dsendexception_0_1.dSendException  "$@" 
--- poms/jobs/process/COMMON/SATS/sat_source_channel_0.1/src/main/templates/Job_run_template.bat
+++
@@ -1,3 +0,0 @@
-%~d0
-cd %~dp0
-java -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/advancedPersistentLookupLib-1.2.jar;../lib/commons-collections-3.2.2.jar;../lib/dom4j-1.6.1.jar;../lib/jboss-serialization.jar;../lib/log4j-1.2.15.jar;../lib/log4j-1.2.16.jar;../lib/mongo-java-driver-3.5.0.jar;../lib/postgresql-8.3-603.jdbc3.jar;../lib/trove.jar;sat_source_channel_0_1.jar; deepsea.sat_source_channel_0_1.SAT_SOURCE_CHANNEL  %* 
